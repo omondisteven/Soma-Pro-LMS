233ce9e9c5f2b33e769865bb926dd77d751463b8
Omondisteven <omondisteven@gmail.com>
2026-04-11 02:33:37 +0300

    Returned ts error checks

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-/** @type {import('next').NextConfig} */
-    const nextConfig = {
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-      eslint: {
-        // Similarly, you can ignore ESLint errors
-        ignoreDuringBuilds: true,
-      },
-    }
+// /** @type {import('next').NextConfig} */
+//     const nextConfig = {
+//       typescript: {
+//         // !! WARN !!
+//         // Dangerously allow production builds to successfully complete even if
+//         // your project has type errors.
+//         // !! WARN !!
+//         ignoreBuildErrors: true,
+//       },
+//       eslint: {
+//         // Similarly, you can ignore ESLint errors
+//         ignoreDuringBuilds: true,
+//       },
+//     }
 
-    module.exports = nextConfig
+//     module.exports = nextConfig
     export default nextConfig;
